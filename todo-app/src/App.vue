730916e448ca58c9a3bcbf985a64ea5ccea09ca6
Eric Jiang <ericjiang97@gmail.com>
2018-06-16 17:44:29 +1000

    more implementations

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,46 +1,62 @@
 <template>
   <div id="app">  
     <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:add-todo="addTodo"></create-todo>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld';
-import TodoList from './components/TodoList'  
+import TodoList from "./components/TodoList";
+import CreateTodo from "./components/CreateToDo";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     TodoList,
+    CreateTodo
+  },
+  methods: {
+    addTodo(title) {
+      this.todos.push({
+        title,
+        done: false
+      });
+    }
   },
   // data function avails data to the template
   data() {
     return {
-      todos: [{
-        title: 'Todo A',
-        project: 'Project A',
-        done: false,
-      }, {
-        title: 'Todo B',
-        project: 'Project B',
-        done: true,
-      }, {
-        title: 'Todo C',
-        project: 'Project C',
-        done: false,
-      }, {
-        title: 'Todo D',
-        project: 'Project D',
-        done: false,
-      }],
+      todos: [
+        {
+          title: "Todo A",
+          project: "Project A",
+          done: false
+        },
+        {
+          title: "Todo B",
+          project: "Project B",
+          done: true
+        },
+        {
+          title: "Todo C",
+          project: "Project C",
+          done: false
+        },
+        {
+          title: "Todo D",
+          project: "Project D",
+          done: false
+        }
+      ]
     };
-  },
+  }
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   /* text-align: center; */
--- todo-app/src/components/ToDoList.vue
+++ todo-app/src/components/ToDoList.vue
@@ -1,16 +1,24 @@
 <template>
   <div>
-    
     <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
     <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
-    <todo  v-for="todo in todos" v-bind:todo="todo"></todo>
-</div>
+    <todo  v-on:delete-todo="deleteTodo" v-for="todo in todos" v-bind:todo="todo"></todo>
+  </div>
 </template>
 
 <script type = "text/javascript" >
-import Todo from "./ToDo.vue";
+import Todo from "./Todo";
 
 export default {
-  props: ["todos"]
+  props: ["todos"],
+  components: {
+    Todo
+  },
+  methods: {
+    deleteTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    }
+  }
 };
 </script>
